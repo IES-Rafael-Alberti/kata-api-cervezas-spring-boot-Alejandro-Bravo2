b3b2f447d664ab7ed092303f03c46e0764f40bfe
Alejandro-Bravo2 <abravoc@iesrafaelalberti.es>
2025-12-14 20:11:54 +0100

    iniciando spring boot

---
+++ kata-cervezas/src/test/java/com/gestioneventos/katacervezas/KataCervezasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestioneventos.katacervezas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataCervezasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
